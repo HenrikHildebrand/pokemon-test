895b5acd491cb435542cd3fefb8a9c209101e01c
Henrik Hildebrand <henrik@distansa.io>
2024-05-09 00:09:59 +0200

    remove non used modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PokemonsModule } from './pokemons/pokemons.module';
 import { PrismaService } from './prisma.service';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [PokemonsModule, EventEmitterModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [PokemonsModule],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
